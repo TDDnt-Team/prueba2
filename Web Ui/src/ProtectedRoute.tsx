a3ab27d6b5ee3ff48a843529101563cc42a638ff
Jose Carlos <jose.lozada@ucb.edu.bo>
2024-10-15 16:03:09 -0400

    Prueba 5

--- Web Ui/src/ProtectedRoute.tsx
+++ Web Ui/src/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { useGlobalState } from "./modules/User-Authentication/domain/authStates";
 import { useNavigate } from "react-router-dom";
 import React, { ReactNode, useEffect } from "react";
+import {VITE_API} from "../config.ts";
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -9,6 +10,7 @@ interface ProtectedRouteProps {
 function ProtectedRouteComponent({ children }: Readonly<ProtectedRouteProps>) {
   const authData = useGlobalState("authData")[0];
   const navigate = useNavigate();
+  console.log("API_URL = ", VITE_API);
   useEffect(() => {
     if (authData.userEmail == "" && authData.userEmail != null) {
       navigate("/login");
